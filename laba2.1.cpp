1f0bdb0761ae884bfe8a3279fada41869515f073
MurikZ <143904061+MurikZ@users.noreply.github.com>
2024-04-22 08:21:20 +0300

    Add files via upload

---
+++ laba2.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    setlocale(0, "RU");
+    string line;
+    int count = 0;
+    int sum = 0;
+
+    ifstream in;
+    in.open("1задание.txt");
+    if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            double n = stod(line);
+            if (n > 0)
+            {
+                count++;
+                sum += n;
+            }
+
+        }
+    }
+    else
+    {
+        cerr << "error\n";
+    }
+
+    cout << "количество положительных" << count;
+    cout << "сумма положительных" << sum;
+    in.close();
+}
