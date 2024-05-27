d94a59432556d9db9ce09ef4a384f4062adce958
MurikZ <143904061+MurikZ@users.noreply.github.com>
2024-05-27 16:32:24 +0300

    Add files via upload

---
+++ laba2.2.2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    setlocale(0, "ru");
+    string line;
+    ifstream in("2задание.txt");
+    int count0 = 0;
+    int count1 = 0;
+
+    while (getline(in, line))
+    {
+        int n = stoi(line);
+        if (n == 0)
+        {
+            count0++;
+        }
+        else if (n == 1)
+        {
+            count1++;
+        }
+    }
+    if (count1 > count0)
+    {
+        cout << "количество единиц больше";
+    }
+    else if (count1 < count0)
+    {
+        cout << "количество нулей больше";
+    }
+    else
+    {
+        cout << "количество нулей и единиц одинаково";
+    }
+    in.close();
+}//задание 2
