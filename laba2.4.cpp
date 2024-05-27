575454b810fab3f47abdbcc4becef5a8493ce672
MurikZ <143904061+MurikZ@users.noreply.github.com>
2024-05-27 16:42:41 +0300

    Add files via upload

---
+++ laba2.4.cpp
@@ -0,0 +1,24 @@
+﻿#include <fstream>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    ifstream input("4задание.txt");
+    int number;
+    int position = 1;
+    int count=0;
+
+    while (input >> number)
+    {
+        if (number == 0 and position % 2 == 0)
+        {
+            count++;
+        }
+        position++;
+    }
+    cout << count;
+    input.close();
+
+    return 0;
+}
