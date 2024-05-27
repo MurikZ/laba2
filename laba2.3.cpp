7d20888d3f72805499632097b31bee9389face2c
MurikZ <143904061+MurikZ@users.noreply.github.com>
2024-05-27 16:37:32 +0300

    Add files via upload

---
+++ laba2.3.cpp
@@ -0,0 +1,22 @@
+﻿#include <fstream>
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+int main()
+{
+    ifstream in("inputзадание3.txt");
+    ofstream out("outputзадание3.txt");
+
+    char c;
+    while (in.get(c))
+    {
+        if (isalpha(c))
+        {
+            out << c;
+        }
+    }
+    in.close();
+    out.close();
+    return 0;
+}
