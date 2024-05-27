be3b6e8a75c8ffa1b9312c5b2fcc031ec7091359
MurikZ <143904061+MurikZ@users.noreply.github.com>
2024-05-27 16:45:50 +0300

    Add files via upload

---
+++ laba2.6.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    vector<char>vet;
+
+    ifstream input("text.txt");
+    char c;
+
+    while (input >> c)
+    {
+        vet.push_back(c);
+    }
+    char First = vet[0];
+    char Second = vet[1];
+    char fc = vet[0] + vet[1];
+    if (isdigit(fc))
+    {
+        int num = fc - '0';
+        if (num % 2 == 0)
+        {
+            cout << "все отлично";
+        }
+        else
+        {
+            cout << "не подходит";
+        }
+    }
+    else
+    {
+        cout << "абсолютно не подходит";
+    }
+    input.close();
+    return 0;
+
+
+}
